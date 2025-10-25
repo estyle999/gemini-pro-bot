ad7a1470012a792ed3fcdd3d4456e5bde0d53a1c
estyle999 <estyle999@gmail.com>
2025-10-25 03:41:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from gemini_pro_bot.bot import start_bot
+from server import start_health_server
 
 if __name__ == "__main__":
-    start_bot()
+    start_health_server()  # Запускаем HTTP сервер для Render
+    start_bot()  # Запускаем бота
