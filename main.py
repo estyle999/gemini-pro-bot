e706474123aec2bd521b9d48edc18878edb4d186
estyle999 <estyle999@gmail.com>
2025-10-26 03:00:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import threading
 from gemini_pro_bot.bot import start_bot
 from server import start_health_server
 
 if __name__ == "__main__":
-    start_health_server()  # Запускаем HTTP сервер для Render
-    start_bot()  # Запускаем бота
+    threading.Thread(target=start_health_server, daemon=True).start()
+    start_bot()
